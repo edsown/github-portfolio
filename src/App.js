a2b2b1f22b35c331915dfe70c69cd6c4692d9461
edsown <edsonoliveira.contato@gmail.com>
2022-01-31 18:21:31 -0300

    Added a title

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import Repos from "./Repos";
 function App() {
   return (
     <div className="App">
+      <br />
+      <br />
+      <h1>My personal projects</h1>
+      <br />
+      <br />
       <Repos />
     </div>
   );
